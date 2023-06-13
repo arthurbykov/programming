2ae83659e5298c05a64f7804897ac830c6931aa1
arthurbykov <136387066+arthurbykov@users.noreply.github.com>
2023-06-14 10:45:53 +1200

    Add files via upload

---
+++ 01_yes_no_v1.py
@@ -0,0 +1,14 @@
+# functions go here
+
+
+# main routine goes here
+want_instructions = input("Do you want to read the instructions? (Y/N): ").lower()Y
+
+if want_instructions == "y":
+    print("Instructions go here")
+elif want_instructions == "n":
+    pass
+else:
+    print("Please answer Y or N")
+
+
